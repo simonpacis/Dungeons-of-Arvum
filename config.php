d711b98e3d6d701de55091b1aff71fc827921cd6
Simon Klit <boutrupsimon@gmail.com>
2017-09-22 14:45:50 +0200

    fix

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $max_players = 4;
 $map_width = 400;
 $map_height = 400;
 
-$generate_new_map = false; //Set to false if you want to load a map from pre-existing map.txt and rooms.txt files in the "libs" folder.
+$generate_new_map = true; //Set to false if you want to load a map from pre-existing map.txt and rooms.txt files in the "libs" folder.
 
 /*
 	In-game specific configuratons
