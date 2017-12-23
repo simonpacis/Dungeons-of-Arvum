44a9a568adfaa5b8bff978b2a93040774ec47d69
Simon Pacis <boutrupsimon@gmail.com>
2017-12-23 16:03:39 +0100

    ideas

---
+++ changes.md
@@ -0,0 +1,10 @@
+4 felter/sekund
+2/sekund
+
+20% bonus til laveste spiller
+
+Ingen damage tekst
+
+Health Potion scalerm, giver 20% af HP
+
+Attack power/Spell power
--- classes/bootstrap.php
+++ classes/bootstrap.php
@@ -6,13 +6,3 @@ include_once('spells/spell.php');
 
 include_all(realpath(dirname(__FILE__)));
 echo "\nGenerating map.\n";
-/*include('player.php');
-include('tile.php');
-include('map/wall.php');
-include('map/floor.php');
-include('map/blank.php');*/
-
-/*include('items/weapons/fjallrung/fjallrung.php');
-include('items/weapons/fjallrung/shardOfFjallRung1.php');
-include('items/weapons/fjallrung/shardOfFjallRung2.php');
-include('items/weapons/fjallrung/shardOfFjallRung3.php');*/
